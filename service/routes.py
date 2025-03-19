236cf38eabe0c22068f1f49a4ec2f435e117bf72
Ummehani18 <ummehani01072002@gmail.com>
2025-03-19 15:48:17 -0400

    fixed build error

--- service/routes.py
+++ service/routes.py
@@ -155,10 +155,11 @@ def check_content_type(media_type):
 
 
 ######################################################################
-#  A C C O U N T   N O T   F O U N D 
+#  A C C O U N T  N O T  F O U N D 
 ######################################################################
 
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
     resp = self.client.get(f"{BASE_URL}/0")
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
